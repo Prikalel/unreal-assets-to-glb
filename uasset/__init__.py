d5f04d9aa173fa08f95ea3300b82020a6f8d21db
Poklonsky Alex <Aleksey.Poklonsky@infotecs.ru>
2026-06-20 08:28:14 +0300

    Add PyPI packaging; version 5.5.0 (UE-versioned)
    - Add pyproject.toml (dynamic version = uasset.__version__), MANIFEST.in, console script 'unreal-assets-to-glb = uasset.cli:main'

--- uasset/__init__.py
+++ uasset/__init__.py
@@ -1,1 +1,7 @@
-"""UE 5.5 UAsset Parser and Exporter package."""
+"""UE 5.5 UAsset Parser and Exporter package.
+
+The distribution version mirrors the Unreal Engine version this build targets
+(see pyproject.toml `dynamic = ["version"]` -> `attr = "uasset.__version__"`).
+"""
+
+__version__ = "5.5.0"
